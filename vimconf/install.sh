d0a8cebda0d04c36a3c514e476999f67c98be1e2
刘飞宇 <feiyu.liu@qunar.com>
2014-10-20 19:38:34 +0800

    delete

--- vimconf/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CURRENT_PATH=$(pwd)
-cp -f ~/.vim ~/.vimbak
-cp ~/.vimrc ~/.vimrcbak
-rm -rf ~/.vim
-rm -f ~/.vimrc
-ln -s $CURRENT_PATH"/vimfiles" ~/.vim
-ln -s $CURRENT_PATH"/_vimrc" ~/.vimrc
-
